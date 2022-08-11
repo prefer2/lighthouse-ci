5b2be3fc3134bd2b6fbf15c3bab9e3e45022304b
prefer2 <sunho6200@gmail.com>
2022-08-11 17:21:13 +0900

    feat: lighyhouserc 수정

--- frontend/.lighthouserc.js
+++ frontend/.lighthouserc.js
@@ -1,11 +1,5 @@
 module.exports = {
   ci: {
-    collect: {
-      staticDistDir: "./build",
-      // startServerCommand: "npm run start", // 서버를 키는 명령어를 통해서도 실행 가능
-      url: ["http://localhost:3000"],
-      numberOfRuns: 5, // lighthouse가 실행되는 횟수
-    },
     upload: {
       target: "filesystem",
       outputDir: "./lhci_reports",
